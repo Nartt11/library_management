8c7d2267f4b02b1f55c2e746962a5641ab70e160
duyngulam <neargamer3103@gmail.com>
2025-11-17 01:07:34 +0700

    merge KT

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider, useAuth } from "@/context/authContext";
-import { DashboardLayout } from "@/components/DashboardLayout";
+// import { DashboardLayout } from "@/components/DashboardLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
